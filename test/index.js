14758c9314438547b379542e956c3fb8f86c8b48
Ben Drucker <bvdrucker@gmail.com>
2014-11-04 08:44:01 -0500

    Test rejection of requests w/out headers

--- test/index.js
+++ test/index.js
@@ -11,10 +11,23 @@ config.set('iron', {
 });
 
 var server = require('../src/server');
+server.pack.register(require('inject-then'), function (err) {
+  if (err) throw err;
+});
+
+var expect = require('chai').expect;
 
 describe('auth', function () {
 
-  it('rejects requests missing an token');
+  it('rejects requests missing an token', function () {
+    return server.injectThen({
+      url: '/'
+    })
+    .then(function (response) {
+      // TODO: should be an auth strategy and be 401
+      expect(response.statusCode).to.equal(400);
+    });
+  });
 
   it('responds with 403 for invalid tokens');
 
